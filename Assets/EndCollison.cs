d46dcb12e27703943fe543f701d916f442808c1d
Ladwan <ladwanhester@gmail.com>
2018-06-12 19:34:06 -0400

    Lose Screen

---
+++ Assets/EndCollison.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndCollison : MonoBehaviour {
+    public Canvas EndGame;
+
+	// Use this for initialization
+	void Start () {
+        EndGame.enabled = false;
+
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+
+
+    
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EndGame.enabled = true;
+    }
+
+}
+
