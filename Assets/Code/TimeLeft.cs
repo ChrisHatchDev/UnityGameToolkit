4f83d15d1087f03d37778118cc926a47f2853047
jacobd300 <JDittrich423@stu.aii.edu>
2018-06-12 18:28:23 -0400

    Final Commit Part 2

---
+++ Assets/Code/TimeLeft.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class TimeLeft : MonoBehaviour
+{
+
+
+    public int timeLeft = 60; //Seconds Overall
+    public Text countdown; //UI Text Object
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+    }
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+
+
+        }
+    }
+}
