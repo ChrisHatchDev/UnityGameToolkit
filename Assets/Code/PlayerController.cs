c533ade489773238a951fd2621064140d25fa80b
Student <Student@aii.stu>
2018-06-12 16:02:41 -0400

    plz god work

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     public float LaneWidth = 1.1f;
-    public float LaneHeight = 2.1f;
+	public float LaneHeight = 2.1f;
     public int NumberOfLanes = 3;
 
     public int Health = 3;
@@ -30,15 +30,15 @@ public class PlayerController : MonoBehaviour {
             {
                 MoveRight();
             }
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                MoveUpward();
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                MoveDown();
-            }
         }
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			MoveUpward();
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			MoveDown();
+		}
 
         HealthText.text = "Health " + Health.ToString();
     }
@@ -52,36 +52,32 @@ public class PlayerController : MonoBehaviour {
             transform.position -= new Vector3(LaneWidth, 0, 0);
 
     }
-    void MoveUpward()
-    {
-        int _movesUpward = (int)(transform.position.y / 2.1f);
-
-
-        if (_movesUpward < +NumberOfLanes / 2)
-            transform.position += new Vector3(0, LaneHeight, 0);
-
-    }
-    void MoveDown()
-    {
-        int _movesDown = (int)(transform.position.y / 2.1f);
-
-
-        if (_movesDown > -NumberOfLanes / 2)
-            transform.position -= new Vector3(0, LaneHeight, 0);
-
-    }
-
 
     void MoveRight()
     {
         int _movesToTheRight = (int)(transform.position.x / 1.1f);
-        //Debug.Log("Moves to the Right =" + _movesToTheRight);
+        
 
         if (_movesToTheRight < NumberOfLanes/3)
             transform.position += new Vector3(LaneWidth, 0, 0);
     }
+	void MoveUpward()
+	{
+		int _movesUpward = (int)(transform.position.y / 2.1f);
 
-    
+
+		if (_movesUpward < NumberOfLanes/3)
+			transform.position += new Vector3 (0, LaneHeight, 0);
+	}
+		
+	void MoveDown()
+	{
+		int _movesDown = (int)(transform.position.y / 2.1f);
+		
+
+		if(_movesDown > -NumberOfLanes/5)
+			transform.position -= new Vector3(0, LaneHeight, 0);
+	}
 
     public void GotHit()
     {
